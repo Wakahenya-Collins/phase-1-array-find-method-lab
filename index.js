969f9deaf301610f924a014f722000dcc5ebe700
Collins Wakahenya <wakahenyacollins@gmail.com>
2023-06-20 13:36:47 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// code your solution here
+// // code your solution here
+// const record = [
+//     
+//     //...
+// ]  // { year: 1997, result: "W" },
+        // { year: 2002, result: "L" },
+        // { year: 2005, result: "L" },
+        // { year: 2007, result: "W" },
+        // { year: 2010, result: "L" },
+
+        function superbowlWin(records) {
+          const winningGame = records.find((game) => game.result === "W");
+        
+          if (winningGame) {
+            return winningGame.year;
+          } else {
+            return undefined;
+          }
+        }
+        
+      
